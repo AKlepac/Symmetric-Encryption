9c2079fc38db3aa66f6004c006b24b05bc45802b
Kieren Pearson <kierenpearson9@gmail.com>
2016-06-11 13:05:17 +1000

    Added return statements to functions
    Also added function prototypes and better testing method of the cypher
    to the main function

--- main.cpp
+++ main.cpp
@@ -4,34 +4,50 @@
 
 using namespace std;
 
+string Encrypt(string text);
+string Decrypt(string text);
+void loadCypher();
+
 string input;
 char cypher[6][5];
 
-void Encrypt(string text)
+string Encrypt(string text)
 {
+	std::string Encrypted;
+
     for (int x = 0; x <= text.length(); x++)
     {
         for (int i = 0; i < 6; i++)
         {
             for (int y = 0; y < 5; y++)
             {
-                if (text[x] == cypher[i][y]){
-                    cout << i << y;
+                if (text[x] == cypher[i][y])
+				{
+					Encrypted.append(std::to_string(i));
+					Encrypted.append(std::to_string(y));
                 }
             }
         }
     }
+
+	return Encrypted;
 };
 
-void Decrypt(string text)
+string Decrypt(string text)
 {
-  for (int i = 0; i < text.length(); i+=2)
-  {
-        int x = text[i] - '0';
+	string Decrypted;
+
+	for (int i = 0; i < text.length(); i+=2)
+	{
+		int x = text[i] - '0';
         int y = text[i+1] - '0';
 
-        cout << cypher[x][y];
-  }
+		char Letter = cypher[x][y];
+
+		Decrypted += Letter;	
+	}
+
+	return Decrypted;
 };
 
 void loadCypher()
@@ -59,5 +75,13 @@ int main()
 {
     loadCypher();
     cin >> input;
+
+	cout << Encrypt(input) << "\n\n";
+
+	cin >> input;	
+
+	cout << Decrypt(input);
+
+	cin >> input;	//somthing to stop the program from just closing
 }
 
